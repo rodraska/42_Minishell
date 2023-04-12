c6b8ad0f04f500d3c6aee9ede9c471e102503499
rodrigo <rreiscarvalho1999@gmail.com>
2023-04-12 17:14:57 +0100

    uhu

--- mini.c
+++ mini.c
@@ -35,43 +35,10 @@ int main(void)
     line = readline("/my_bash: ");
     while (line != NULL)
     {
+        add_history(line);
         ft_tokens(line);
         free(line);
         line = readline("/my_bash: ");
     }
-    /* tokens = ft_split(line, ' ');
-    i = -1;
-    while (tokens[++i])
-        token_add_back(&tkn, tokens[i]);
-    ft_chdir("folder");
-    printf("%s\n", ft_cwd());
-    printf("%s\n", ft_env_var("HOME")); */
-    //token_print(tkn);
     return (0);
 }
-
-/* int main(void)
-{
-    char *line;
-    t_token *tkn;
-    int i;
-
-    i = 0;
-    tkn = NULL;
-    line = readline("/my_bash: ");
-    if (strlen(line) > 0)
-            add_history(line);
-    token_add_back(&tkn, line);
-    while (i < 5)
-    {
-        line = readline("/my_bash: ");
-        if (line != NULL && strlen(line) > 0)
-        {
-            token_add_back(&tkn, line);
-            add_history(line);
-        }
-        i++;
-    }
-    token_print(tkn);
-    return (0);
-} */
